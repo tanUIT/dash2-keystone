560e48ec96262b7513ddc5ccbcae0ed0b407c3c3
Duy Tan <duytan.uit@gmail.com>
2015-10-31 22:08:00 +0700

    update function

--- routes/index.js
+++ routes/index.js
@@ -19,12 +19,7 @@
  */
 
 var keystone = require('keystone');
-var middleware = require('./middleware');
 var importRoutes = keystone.importer(__dirname);
-
-// Common Middleware
-keystone.pre('routes', middleware.initLocals);
-keystone.pre('render', middleware.flashMessages);
 
 // Import Route Controllers
 var routes = {
@@ -36,9 +31,9 @@ exports = module.exports = function(app) {
 	
 	// Views
 	app.get('/', routes.views.index);
-	app.get('/blog/:category?', routes.views.blog);
-	app.get('/blog/post/:post', routes.views.post);
 	app.all('/contact', routes.views.contact);
+	app.get('/category/:category?', routes.views.index);
+	app.get('/post/:post', routes.views.post);
 	
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
